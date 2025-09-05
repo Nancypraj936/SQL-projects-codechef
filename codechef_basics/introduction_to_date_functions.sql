b274e1197a435cd2b16718acd4314286a89b1681
Nancypraj936 <139212561+Nancypraj936@users.noreply.github.com>
2025-09-05 18:55:40 +0530

    Create introduction_to_date_functions.sql

---
+++ codechef_basics/introduction_to_date_functions.sql
@@ -0,0 +1,2 @@
+SELECT DATETIME(); --prints date &time (UTC as default - Coordinated Universal Time)
+SELECT DATETIME('now', '+5 hours', '+30 minutes'); -- prints IST - Indian Standard Time( which is 5hr and 30mins ahead of UTC)
